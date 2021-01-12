f48fc7fa6bdf026e3c273764b591ada2d272bd81
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-12 11:44:09 +0500

    Add files via upload

---
+++ creating funtions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+void counting()
+{for(int i=100;i>0;i--)
+cout<<i<<endl;
+}
+int main()
+{cout<<"My name is Daniyal Akram."<<endl;
+counting();
+cout<<"I am 18 years old."<<endl;
+counting();
+}
